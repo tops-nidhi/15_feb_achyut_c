699f727f8fb490a8ae3379ae46ab6c0fac14d5e0
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-02-23 11:13:55 +0530

    Add files via upload

---
+++ Task/escape_sequence.c
@@ -0,0 +1,25 @@
+//Write a program to use escape sequence
+#include<stdio.h>
+
+int main()
+{
+    // use of \n
+    printf("Address\nVillage: Dholidhar \n");
+    
+    // use of \t
+    printf("\tTaluka: Jam Kandorana \n");
+    
+    // use of \r
+    printf("District:\rRajkot \n");
+
+    // use of \b
+    printf("Gujrat\b-360405 \n");
+
+    // use of \f
+    printf("what is programme\f-it is a set of instruction \n");
+
+    // use of \v
+    printf("what is programming\v-to creat a program \n");
+
+    return 0;
+}
