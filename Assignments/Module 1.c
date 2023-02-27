ab1f4de66ed672bd99224779e905d55729910e7f
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-02-27 19:08:27 +0530

    Update Module 1.c

--- Assignments/Module 1.c
+++ Assignments/Module 1.c
@@ -4,6 +4,6 @@ int main()
     printf("Name: Achyut M. Padariya \n");
     printf("Date of Birth: 1st February 2001 \n");
     printf("Age: 22 Years \n");
-    printf("Address: Village-Dholidhar,Taluka: Jam Kandorana, District: Rajkot, Gujrat-360405 \n");
+    printf("Address: Village-Dholidhar,Taluka-Jam Kandorana, District-Rajkot, Gujrat-360405 \n");
     return 0;
 }
