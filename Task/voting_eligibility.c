f55b89bfaaea81d2b2dc2ce670e53657db1dd0b0
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-03-02 13:54:05 +0530

    Add files via upload

---
+++ Task/voting_eligibility.c
@@ -0,0 +1,22 @@
+// WAP to get input birth year and find he/she is eligible for vote or not
+#include <stdio.h>
+
+int main() 
+{
+    int age;
+    printf("Enter your age : ");
+    scanf("%d",&age);
+    age = 2023 - age;
+    
+    if (age >= 18)
+    {
+        printf("You are eligible for vote");
+    }
+    
+    else
+    {
+        printf("You are not eligible for vote");
+    }
+    
+    return 0;
+}
