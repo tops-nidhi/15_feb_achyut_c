d3bf7cf5ec35466a98005d1cae064d1d640e2ba6
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-03-09 00:12:48 +0530

    Add files via upload

---
+++ Assignments/Module (3)/m(3) percentage_class.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    float maths,gujrati,science,english;
+    
+    printf("Enter the marks of maths : ");
+    scanf("%f",&maths);
+    printf("Enter the marks of gujrati : ");
+    scanf("%f",&gujrati);
+    printf("Enter the marks of science : ");
+    scanf("%f",&science);
+    printf("Enter the marks of english : ");
+    scanf("%f",&english);
+    
+    float percentage = (maths + gujrati + science + english) / 400 * 100;
+    printf("Your Percentage is %f\n",percentage);
+    
+    if(percentage>75)
+    printf("Pass With Distinction");
+    
+    else if(percentage>60 && percentage<=75)
+    printf("Pass With First Class");
+    
+    else if(percentage>50 && percentage<=60)
+    printf("Pass With Second Class");
+    
+    else if(percentage>35 && percentage<=50)
+    printf("Just Pass Class");
+    
+    else
+    printf("Fail");
+    
+    return 0;
+}
---
+++ Assignments/Module (3)/m(3) prime_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    
+    if (num == 2 || num == 3 || num == 5 || num == 7){
+        printf("Given number is prime number");
+    }
+    
+    else if (num%2==0 || num%3==0 || num%4==0 || num%5==0 || num%7==0){
+        printf("Given number is not prime number");
+    }
+    
+    else{
+        printf("Given number is prime number");
+    }
+    return 0;
+}
