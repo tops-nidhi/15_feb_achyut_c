7295a76bde3407bea76932e26e94e017ca6168d1
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-03-09 00:08:12 +0530

    Add files via upload

---
+++ Assignments/Module (1)/Module 1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    printf("Name: Achyut M. Padariya \n");
+    printf("Date of Birth: 1st February 2001 \n");
+    printf("Age: 22 Years \n");
+    printf("Address: Village-Dholidhar,Taluka: Jam Kandorana, District: Rajkot, Gujrat-360405 \n");
+    return 0;
+}
