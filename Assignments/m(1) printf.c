be28d8206418c65696fb8afc1ebbdd1da1bb37c1
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-03-09 00:11:11 +0530

    Delete m(1) printf.c

--- Assignments/m(1) printf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() 
-{
-    printf("Name: Achyut M. Padariya \n");
-    printf("Date of Birth: 1st February 2001 \n");
-    printf("Age: 22 Years \n");
-    printf("Address: Village-Dholidhar,Taluka: Jam Kandorana, District: Rajkot, Gujrat-360405 \n");
-    return 0;
-}
