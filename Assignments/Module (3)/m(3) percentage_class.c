2c6b167c6c01262663214f8d52786e452935a8ed
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-03-09 10:03:33 +0530

    Update m(3) percentage_class.c

--- Assignments/Module (3)/m(3) percentage_class.c
+++ Assignments/Module (3)/m(3) percentage_class.c
@@ -12,23 +12,29 @@ int main() {
     printf("Enter the marks of english : ");
     scanf("%f",&english);
     
-    float percentage = (maths + gujrati + science + english) / 400 * 100;
+    float percentage = (maths + gujrati + science + english) / 400 * 100;{
     printf("Your Percentage is %f\n",percentage);
+    }
     
-    if(percentage>75)
+    if(percentage>75){
     printf("Pass With Distinction");
+    }
     
-    else if(percentage>60 && percentage<=75)
+    else if(percentage>60 && percentage<=75){
     printf("Pass With First Class");
+    }
     
-    else if(percentage>50 && percentage<=60)
+    else if(percentage>50 && percentage<=60){
     printf("Pass With Second Class");
+    }
     
-    else if(percentage>35 && percentage<=50)
+    else if(percentage>35 && percentage<=50){
     printf("Just Pass Class");
+    }
     
-    else
+    else{
     printf("Fail");
+    }
     
     return 0;
 }
