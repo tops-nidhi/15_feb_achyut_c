e8e478e8245c11e3f2840165fb94dd4e4f3a0817
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-03-13 23:36:00 +0530

    Add files via upload

---
+++ Assignments/Module (3)/m(3) find area_using switch.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int find;
+    printf("1. Area of triangle\n2. Area of rectangle\n3. Area of circle\n");
+    printf("\nEnter what you want to find : ");
+    scanf("%d",&find);
+    
+    switch (find){
+        case 1:
+        float triangle,base,height;
+        printf("\nEnter base : ");
+        scanf("%f",&base);
+        printf("Enter height : ");
+        scanf("%f",&height);
+        triangle = (base * height) * 1 / 2;
+        printf("\nArea of triangle is : %f",triangle);
+        break;
+        
+        case 2:
+        float rectangle,length,width;
+        printf("\nEnter length : ");
+        scanf("%f",&length);
+        printf("Enter width : ");
+        scanf("%f",&width);
+        rectangle = length * width;
+        printf("\nArea of rectangle is : %f",rectangle);
+        break;
+        
+        case 3:
+        float circle,radius;
+        printf("\nEnter radius of circle : ");
+        scanf("%f",&radius);
+        circle = 3.14 * (radius * radius);
+        printf("\nArea of circle is : %f",circle);
+        break;
+        
+        default:
+        printf("\nThis action is not available, please enter from given actions....");
+        break;
+    }
+    return 0;
+}
