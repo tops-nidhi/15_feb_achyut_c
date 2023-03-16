dbaeb576ed0f30f15c2902927541ad0b233f3cd3
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-03-16 10:12:48 +0530

    Update arithmatic without_third variable.c

--- Task/arithmatic without_third variable.c
+++ Task/arithmatic without_third variable.c
@@ -2,8 +2,10 @@
 int main()
 {
     int a,b;
-    printf("Enter the value of 2 numbers : ");
-    scanf("%d%d" ,&a ,&b);
+    printf("Enter the first number : ");
+    scanf("%d" ,&a);
+    printf("Enter the second number : ");
+    scanf("%d" ,&b);
     printf("\nAddition of two numbers :  %d" ,a+b);
     printf("\nSubstraction of 2 numbers : %d" ,a-b);
     printf("\nMultiplication of 2 numbers : %d" ,a*b);
