35b1565fdbe34333d5409c715b1f69db039280aa
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-04-12 22:03:42 +0530

    Add files via upload

---
+++ Assignments/Module (3)/m(3) number_pyramid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of lines : ");
+    scanf("%d",&n);
+    
+    int sum = 1;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("%d ",sum);
+            sum++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
