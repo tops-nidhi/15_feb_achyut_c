d6f0c625234da19126f7c9dc454ce6590b98fcbc
achyutam-21 <125861102+achyutam-21@users.noreply.github.com>
2023-04-18 21:49:22 +0530

    Add files via upload

---
+++ Assignments/Module (3)/m(3) marks_percentage.c
@@ -0,0 +1,36 @@
+// WAP to enter the 5 subjects mark. You have to make a total and find the percentage.
+
+#include <stdio.h>
+
+int main() {
+    float maths,gujrati,science,english;
+    
+    printf("Enter the marks of maths : ");
+    scanf("%f",&maths);
+    printf("Enter the marks of gujrati : ");
+    scanf("%f",&gujrati);
+    printf("Enter the marks of science : ");
+    scanf("%f",&science);
+    printf("Enter the marks of english : ");
+    scanf("%f",&english);
+    
+    float percentage = (maths + gujrati + science + english) / 400 * 100;
+    printf("Percentage is %f\n",percentage);
+    
+    if(percentage>75)
+    printf("Pass With Distinction");
+    
+    else if(percentage>60 && percentage<=75)
+    printf("Pass With First Class");
+    
+    else if(percentage>50 && percentage<=60)
+    printf("Pass With Second Class");
+    
+    else if(percentage>35 && percentage<=50)
+    printf("Just Pass Class");
+    
+    else
+    printf("Fail");
+    
+    return 0;
+}
